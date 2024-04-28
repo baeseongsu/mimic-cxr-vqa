984da408fc5421d8f3718c58859b9048077f190a
baeseongsu <subae94@gmail.com>
2024-04-29 00:18:30 +0900

    Update prepare_data.sh

--- mimiccxrvqa/exp/prepare_data.sh
+++ mimiccxrvqa/exp/prepare_data.sh
@@ -8,7 +8,6 @@
 # image preprocessing
 python prepare_data/resize_images.py \
 --mimic_cxr_jpg_dir "../../physionet.org/files/mimic-cxr-jpg/2.0.0" \
---mimic_cxr_jpg_dir "/home/data_storage/mimic-cxr-jpg/2.0.0/" \
 --chest_imagenome_dir "../../physionet.org/files/chest-imagenome/1.0.0/" \
 --save_img_dir "../../physionet.org/files/mimic-cxr-jpg/2.0.0/re512_3ch_contour_cropped" \
 --resolution 512 \
